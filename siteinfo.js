c9e119d999dff12033d418780a7f14d235084a1a
thanatos <thanatos915@163.com>
2018-11-11 14:09:15 +0800

    申请提现

--- siteinfo.js
+++ siteinfo.js
@@ -1,8 +1,10 @@
 var t = {
     acid: -1,
     version: "1.0.0",
-    siteroot: "http://api.qdvmai.com",
-    apiroot: "http://api.qdvmai.com/web/index.php?store_id=1&r=api/"
+    // siteroot: "http://api.qdvmai.com",
+    siteroot: "http://api.shop.com",
+    // apiroot: "http://api.qdvmai.com/web/index.php?store_id=1&r=api/"
+    apiroot: "http://api.shop.com/web/index.php?store_id=1&r=api/"
 };
 
 module.exports = t;
