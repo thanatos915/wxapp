dbfe53930039c81537dda2071400f006a0ec4fb3
thanatos <thanatos915@163.com>
2018-12-03 14:52:17 +0800

    BUG

--- siteinfo.js
+++ siteinfo.js
@@ -1,9 +1,9 @@
 var t = {
     acid: -1,
     version: "1.0.0",
-    siteroot: "https://www.zhongmai.pro",
+    siteroot: "https://zs.qdvmai.com",
     // siteroot: "http://api.shop.com",
-    apiroot: "https://www.zhongmai.pro/web/index.php?store_id=1&r=api/"
+    apiroot: "https://zs.qdvmai.com/web/index.php?store_id=1&r=api/"
     // apiroot: "http://api.shop.com/web/index.php?store_id=1&r=api/"
 };
 
